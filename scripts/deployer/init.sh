ebb55ec2e6855cb5e83ef691ea2831353b513112
Yasi <wyasirurathsara@gmail.com>
2023-02-22 20:28:08 +0100

    :construction: syncer and args for data msgs

--- scripts/deployer/init.sh
+++ scripts/deployer/init.sh
@@ -26,7 +26,7 @@ for label in "${labels[@]}"; do
     break
   fi
 
-  screen -d -m -S "$label" ./didcomm-prober -label="${labels[$counter]}" -port="${ports[$counter]}" -pub="${pub_ports[$counter]}" -mock_port="${mock_ports[$counter]}" -buf="$buf" "$mode" -v
+  screen -d -m -S "$label" ./didcomm-prober -label="${labels[$counter]}" -port="${ports[$counter]}" -pub="${pub_ports[$counter]}" -mock_port="${mock_ports[$counter]}" -buf="$buf" -v -sync "$mode"
 #  screen -d -m -S "$label" ssh -i "$key_path" "$user@${ips[$counter]}" "cd agent/ && ./didcomm-prober -label=${labels[$counter]} -port=${ports[$counter]} -pub=${pub_ports[$counter]} -mock_port=${mock_ports[$counter]} -buf="$buf" "$mode" -v"
 
   node="${ips[$counter]}:${mock_ports[$counter]}"
